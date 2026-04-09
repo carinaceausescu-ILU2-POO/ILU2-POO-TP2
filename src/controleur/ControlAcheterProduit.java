238e5ad1b8cd5a127789aa5cede94092f107c9b3
carinaceausescu <anacarina.ceausescu@gmail.com>
2026-04-09 19:45:14 +0200

    .

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -14,6 +14,15 @@ public class ControlAcheterProduit {
 		this.controlVerifierIdentite = controlVerifierIdentite;
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
+	
+	public boolean verifierIdentite(String nomAcheteur) {
+		return controlVerifierIdentite.verifierIdentite(nomAcheteur);
+	}
+	
+	public boolean existeVendeursProduit(String produit) {
+	    return village.rechercherVendeursProduit(produit) != null;
+	}
+	
+	
 
-	//TODO a completer
 }
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -10,6 +10,21 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO à completer
+		boolean nomAcheteurConnu = controlAcheterProduit.verifierIdentite(nomAcheteur);
+		
+		if(!nomAcheteurConnu) {
+			System.out.println("Je suis désolée " +  nomAcheteur
+					+ " mais il faut être un habitant de notre village pour commercer ici.");
+		} else {
+			String question = "Quel produit voulez-vous acheter ?";
+			String produit = Clavier.entrerChaine(question);
+			
+			if (!controlAcheterProduit.existeVendeursProduit(produit)) {
+			    System.out.println("Désolé, personne ne vend ce produit au marché.");
+			    return;
+			} else {
+				
+			}
+		}
 	}
 }
